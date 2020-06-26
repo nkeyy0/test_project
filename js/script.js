25874651f3fe28083d18031cc251c5cd7ca78e88
Nikita <komkovnk7@gmail.com>
2020-06-26 23:35:30 +0300

    fix version_1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "0");
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "0");
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +10,12 @@ let personalMovieDB = {
     privat: false
 };
 
-let lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-let rating = prompt("На сколько оцените его?", "");
+let lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+    rating = +prompt("На сколько оцените его?", ""),
+    lastFilm1 = prompt("Один из последних просмотренных фильмов?", ""),
+    rating1 = +prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies = {
-    lastFilm,
-    rating
-};
+personalMovieDB.movies[lastFilm] = rating;
+personalMovieDB.movies[lastFilm1] = rating1; 
 
 console.log(personalMovieDB.movies);
