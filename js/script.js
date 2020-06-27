d659fd3287d4eb40881ba71af6a3f93981a99ec5
Nikita <komkovnk7@gmail.com>
2020-06-27 04:29:29 +0300

    add cycles and terms(correct)

--- js/script.js
+++ js/script.js
@@ -15,16 +15,17 @@ if(personalMovieDB.count <= 10 && personalMovieDB.count > 0) {
 }
 else if(personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
     alert("Вы классический зритель");
-} else {
+} else  if (personalMovieDB.count > 30) {
     alert("Вы киноман!");
+} else {
+    alert("Произошла обишка");
 }
-
 
 
 let num = 1;
 while(num <=2) {
     let lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-    if(lastFilm != '' && lastFilm.length < 50) {
+    if(lastFilm != null && lastFilm.length != '' && lastFilm.length < 50) {
         let rating = prompt("На сколько оцените его?", "");
         personalMovieDB.movies[lastFilm] = rating;
         num++;
