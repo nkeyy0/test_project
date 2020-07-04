b041c5a3b73a1d2cf92d1b77a81c8988d2c753d7
Nikita <komkovnk7@gmail.com>
2020-07-04 17:20:17 +0300

    add function in object

--- js/script.js
+++ js/script.js
@@ -19,25 +19,20 @@ let personalMovieDB = {
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-
-function detectPersonalLevel () {
-    if(personalMovieDB.count <= 10 && personalMovieDB.count > 0) {
-        alert("Просмотренно мало фильмов");
-    }
-    else if(personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
-        alert("Вы классический зритель");
-    } else  if (personalMovieDB.count > 30) {
-        alert("Вы киноман!");
-    } else {
-        alert("Произошла обишка");
-    }
-}
-
-detectPersonalLevel();
-
-function rememberMyFilms() {
+    privat: false,
+    detectPersonalLevel: function(){
+        if(personalMovieDB.count <= 10 && personalMovieDB.count > 0) {
+            alert("Просмотренно мало фильмов");
+        }
+        else if(personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+            alert("Вы классический зритель");
+        } else  if (personalMovieDB.count > 30) {
+            alert("Вы киноман!");
+        } else {
+            alert("Произошла обишка");
+        }
+    },
+    rememberMyFilms: function() {
     let num = 1;
     while(num <=2) {
     let lastFilm = prompt("Один из последних просмотренных фильмов?", "");
@@ -45,30 +40,47 @@ function rememberMyFilms() {
     if(lastFilm != null && rating !=null && rating !='' && lastFilm.length != '' && lastFilm.length < 50) {
         personalMovieDB.movies[lastFilm] = rating;
         num++;
+        }
     }
-}
-}
+    },
+    showMyDB: function(callback){
+        callback();
+        if(personalMovieDB.privat == false) {
+            console.log(personalMovieDB);
+        } else {
+            console.log("Error");
+        } 
+    },
+    /*
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+    writeYourGenres: function() {
+        for(let i = 0; i < 3; i++) {
+            personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`);
+            while(personalMovieDB.genres[i] == null || personalMovieDB.genres[i] == ''){
+                personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`);
+            }
+        }
+        personalMovieDB.genres.forEach(function(item, i) {
+            console.log(`Любимый жанр №{$i} это ${item}`);
+        });
+    },
+    toggleVisibleMyDB: function() {
+        if(personalMovieDB.privat == false){
+            personalMovieDB.privat = true;
+        }
+        else {
+            personalMovieDB.privat = false;
+        }
+    }
+};
 
-rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
 
-function showMyDB () {
-    if(personalMovieDB.privat == false) {
-        console.log(personalMovieDB);
-    } else {
-        console.log("Error");
-    } 
-}
+personalMovieDB.rememberMyFilms();
 
-showMyDB();
+personalMovieDB.writeYourGenres();
 
-// Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-// "Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-// 
-function writeYourGenres() {
-    for(let i = 0; i < 3; i++) {
-        let genre = prompt(`Ваш любимый жанр под номером ${i+1}`);
-        personalMovieDB.genres[i] = genre;
-    }   
-}
-
-writeYourGenres();
+personalMovieDB.showMyDB(personalMovieDB.toggleVisibleMyDB);
